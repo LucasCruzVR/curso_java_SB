6898ec15afab43ed546654a632a6816fd11b2c90
lucas cruz <lucas_cruz100@live.com>
2022-08-28 15:42:44 -0300

    endpoint usuarios/id

---
+++ src/main/java/com/projetoaula/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.projetoaula.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoaula.domain.Categoria;
+import com.projetoaula.domain.Cliente;
+import com.projetoaula.repositories.CategoriaRepository;
+import com.projetoaula.repositories.ClienteRepository;
+import com.projetoaula.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> {
+			throw new ObjectNotFoundException("Cliente não encontrada! ID: " + id);
+		});
+	}
+}
