02b9b7235723682779f0c47ac2766e683e11442e
lucas.rodrigues <lucas.rodrigues@goias.gov.br>
2022-08-29 08:17:39 -0300

    endpoint pedidos

---
+++ src/main/java/com/projetoaula/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.projetoaula.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoaula.domain.Pedido;
+import com.projetoaula.repositories.PedidoRepository;
+import com.projetoaula.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	@Autowired
+	private PedidoRepository repo;
+	
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> {
+			throw new ObjectNotFoundException("Pedido não encontrada! ID: " + id);
+		});
+	}
+}
