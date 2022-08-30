bc36b3d60bb198d1eba7f7a10282483166f6c1fa
lucas cruz <lucas_cruz100@live.com>
2022-08-30 13:35:55 -0300

    put, get e delete para clientes

--- src/main/java/com/projetoaula/resources/ClienteResource.java
+++ src/main/java/com/projetoaula/resources/ClienteResource.java
@@ -1,13 +1,21 @@
 package com.projetoaula.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoaula.domain.Cliente;
+import com.projetoaula.domain.Cliente;
+import com.projetoaula.dto.ClienteDTO;
 import com.projetoaula.services.ClienteService;
 
 @RestController
@@ -21,5 +29,27 @@ public class ClienteResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Integer id, @Valid @RequestBody ClienteDTO obj) {
+		Cliente cliente = service.fromDTO(obj);
+		cliente.setId(id);
+		cliente = service.update(cliente);
+		return ResponseEntity.ok().body(cliente);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> clientes = service.findAll();
+		List<ClienteDTO> clientesDTO = clientes.stream().map(cliente -> new ClienteDTO(cliente))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(clientesDTO);
 	}
 }
--- src/main/java/com/projetoaula/services/ClienteService.java
+++ src/main/java/com/projetoaula/services/ClienteService.java
@@ -1,25 +1,63 @@
 package com.projetoaula.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.projetoaula.domain.Categoria;
 import com.projetoaula.domain.Cliente;
-import com.projetoaula.repositories.CategoriaRepository;
+import com.projetoaula.dto.ClienteDTO;
 import com.projetoaula.repositories.ClienteRepository;
+import com.projetoaula.services.exceptions.DataIntegrityException;
 import com.projetoaula.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> {
 			throw new ObjectNotFoundException("Cliente não encontrada! ID: " + id);
 		});
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException err) {
+			throw new DataIntegrityException("Não é possível excluir uma Cliente que tenha Pedidos vinculados.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> pageClientes(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente novoCliente, Cliente clienteParams) {
+		novoCliente.setNome(clienteParams.getNome());
+		novoCliente.setEmail(clienteParams.getEmail());
 	}
 }
