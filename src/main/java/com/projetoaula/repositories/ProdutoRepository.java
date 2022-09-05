bd0b8efc595f5ea2a20f3db5f926a090401073ec
lucas <lucas_cruz100@live.com>
2022-09-05 15:13:54 -0300

    pesquisa de produtos

--- src/main/java/com/projetoaula/repositories/ProdutoRepository.java
+++ src/main/java/com/projetoaula/repositories/ProdutoRepository.java
@@ -1,11 +1,32 @@
 package com.projetoaula.repositories;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.projetoaula.domain.Categoria;
 import com.projetoaula.domain.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-	
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	// para sql nativo usar (nativy query = true)
+	@Transactional
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat where obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+
+	/*
+	 * EXEMPLO
+	 * 
+	 * @Query(value = "select distinct on (bf.type_fee) * " +
+	 * "from dbo.basket_fee bf " + "where bf.type_fee = :type or (:type is null) " +
+	 * "order by bf.type_fee , bf.created_date desc", nativeQuery = true)
+	 * Page<BasketFee> findAll(@Param("type") FeeType type, Pageable pageable);
+	 */
 }
